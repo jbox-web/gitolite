e5c36a89e93dd69ce68c567579cd28e8f1c3bc22
Nicolas <nrodriguez@jbox-web.com>
2014-04-29 00:51:00 +0200

    Test if commit works

--- spec/gitolite_admin_spec.rb
+++ spec/gitolite_admin_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Gitolite::GitoliteAdmin do
 
+  conf_dir   = File.join(File.dirname(__FILE__), 'fixtures', 'configs')
   output_dir = '/tmp'
   # output_dir = File.join(File.dirname(File.dirname(__FILE__)), 'tmp')
 
@@ -29,6 +30,23 @@ describe Gitolite::GitoliteAdmin do
     it 'should detect a non gitolite-admin repository' do
       test_dir = output_dir
       expect(GitoliteAdmin.is_gitolite_admin_repo?(test_dir)).to be false
+    end
+  end
+
+  describe '#save' do
+    it 'should commit file to gitolite-admin repository' do
+      test_dir = File.join(output_dir, 'gitolite-admin-test')
+      opts = { :overwrite => true }
+      gl_admin = GitoliteAdmin.bootstrap(test_dir, opts)
+
+      c = Gitolite::Config.new(File.join(conf_dir, 'complicated.conf'))
+      c.filename = 'gitolite.conf'
+
+      gl_admin.config = c
+      gl_admin.save
+
+      new_file = File.join(test_dir, 'conf', c.filename)
+      expect(File.file?(new_file)).to be true
     end
   end
 
