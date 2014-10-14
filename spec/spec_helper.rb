86a596023b3e2ff49d4fa63d469e169ce837312b
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2014-10-14 03:32:02 +0200

    Add Coverall and Codeclimate report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,15 @@ require 'forgery'
 
 require 'simplecov'
 require 'simplecov-rcov'
+require 'coveralls'
+require 'codeclimate-test-reporter'
 
 ## Configure SimpleCov
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
-  SimpleCov::Formatter::RcovFormatter
+  SimpleCov::Formatter::RcovFormatter,
+  Coveralls::SimpleCov::Formatter,
+  CodeClimate::TestReporter::Formatter
 ]
 
 SimpleCov.start
